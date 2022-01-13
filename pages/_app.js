71bfe07a33a29bae1fd0e8187226e53b8fd9c4e7
NEWUSER <webdev.hemant@gmail.com>
2022-01-13 21:40:29 +0530

    build complete

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
           rel="stylesheet"
           href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
           integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></link>
       </Head>
       <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,14 @@ import Profile from "../components/Profile";
 const index = () => {
   return (
     <div>
-      <h1 className="bg-primary">hellow</h1>
+      <h1 className="">Something Amazing is comming!!</h1>
       <Container fluid="md">
-        <Row>
-          <Col>1 of 1</Col>
-        </Row>
+        <Row>{/* <Col>1 of 1</Col> */}</Row>
       </Container>
       <div className="container">
         <div className="row">
           <div className="col-5 mx-auto">
-            <div className="lund bg-secondary">hi</div>
+            {/* <div className="lund bg-secondary">hi</div> */}
             {/* {[
               "primary",
               "secondary",
@@ -28,7 +26,7 @@ const index = () => {
                 This is a {variant} alert—check it out!
               </Alert>
             ))} */}
-            <Alert variant="success">hi betichod</Alert>
+            {/* <Alert variant="success">hi betichod</Alert> */}
           </div>
         </div>
       </div>
