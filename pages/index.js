54b539fb31a5080866c0f1aa969b9208a79a13ad
NEWUSER <webdev.hemant@gmail.com>
2022-01-14 23:15:08 +0530

    typical

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import { Alert, Col, Container, Row } from "react-bootstrap";
 import Typical from "react-typical";
 import style from "../styles/Home.module.scss";
 import NavBar from "../components/navbar/NavBar";
+import Intro from "../components/introduction/Intro";
 const index = () => {
   return (
     <div className="Home ">
       <header className={style.header}>
         <NavBar />
+        <Intro />
       </header>
     </div>
   );
