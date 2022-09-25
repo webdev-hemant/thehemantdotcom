e22eca5d23745a1a789afa0dcb1617e8da3b379c
webdev-hemant <webdev.hemant@gmail.com>
2022-09-25 13:17:40 +0530

    setup

---
+++ src/modules/home/Homemodules.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './homemodule.module.scss'
+const Homemodules = () => {
+    return (
+        <div>Homemodules</div>
+    )
+}
+
+export default Homemodules
