2bb1354dc3c01b006721b75cab324530cff5d6a4
webdev-hemant <webdev.hemant@gmail.com>
2022-09-29 14:51:02 +0530

    footer added

--- src/modules/home/firstHomeSection/index.tsx
+++ src/modules/home/firstHomeSection/index.tsx
@@ -26,7 +26,7 @@ const FirstHomeSection = () => {
         </div>
         <p className={styles.introParagraph}>
           I solve customers problem by giving software services and follow only
-          one strategy - Be so good at it that they can't ignore you!
+          one strategy - Be so good at it that they can&apos;t ignore you!
         </p>
         <SocialMediaComponents />
         <Link href="/contact_me">
