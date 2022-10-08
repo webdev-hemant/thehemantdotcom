a87b2bee48bb5423d4edf84b53c19d108b35c3e3
webdev-hemant <webdev.hemant@gmail.com>
2022-10-08 18:05:51 +0530

    text updated

--- src/modules/home/ThirdHomeSection/index.tsx
+++ src/modules/home/ThirdHomeSection/index.tsx
@@ -7,8 +7,10 @@ const ThirdHomeSection = () => {
         <div className={styles.textContainer}>
           <h2 className={styles.textHeading}>
             &quot;Being an{" "}
-            <i style={{ borderBottom: "3px solid white" }}>FullStack</i> is the
-            best way to experience super-human power in real life!&quot;
+            <i style={{ borderBottom: "3px solid white" }}>
+              FullStack developer
+            </i>{" "}
+            is the best way to experience super-human power in real life!&quot;
             <span className={styles.credit}> — Hemant Jadhav</span>
           </h2>
         </div>
