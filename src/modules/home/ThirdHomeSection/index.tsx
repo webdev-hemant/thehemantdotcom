1b8de4579b9f1d85e72fa668c619b465b6bd704d
webdev-hemant <webdev.hemant@gmail.com>
2022-10-08 17:57:34 +0530

    third section complete

--- src/modules/home/ThirdHomeSection/index.tsx
+++ src/modules/home/ThirdHomeSection/index.tsx
@@ -3,7 +3,16 @@ import styles from "./thirdhomesection.module.scss";
 const ThirdHomeSection = () => {
   return (
     <section className={styles.thirdSectionWrapperr}>
-      <div className={styles.imageContainer}></div>
+      <div className={styles.imageContainer}>
+        <div className={styles.textContainer}>
+          <h2 className={styles.textHeading}>
+            &quot;Being an{" "}
+            <i style={{ borderBottom: "3px solid white" }}>FullStack</i> is the
+            best way to experience super-human power in real life!&quot;
+            <span className={styles.credit}> — Hemant Jadhav</span>
+          </h2>
+        </div>
+      </div>
     </section>
   );
 };
