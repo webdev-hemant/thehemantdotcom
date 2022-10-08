d912acfb263fe53af407a672ef05b07761598340
webdev-hemant <webdev.hemant@gmail.com>
2022-10-08 21:44:38 +0530

    navigation removed

--- src/modules/education/Education.tsx
+++ src/modules/education/Education.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
-// import educationImg from "images/college.png";
-import educationImg from "images/seminar.png";
+import collegeimg from "images/collegeimg.png";
+// import educationImg from "images/seminar.png";
 import styles from "./education.module.scss";
 
 const Education = () => {
   return (
     <div className={styles.educationWrapper}>
-      <div className={styles.imgContainer}>
-        <Image src={educationImg} alt="" layout="fill" objectFit="contain" />
+      <div className={styles.colloegInfoContainer}>
+        <div className={styles.imgContainer}>
+          <Image src={collegeimg} alt="" layout="fill" objectFit="contain" />
+        </div>
+        <div className={styles.collegeText}>
+          <h3 className={styles.clgName}>
+            Vighnaharata Trust&apos;s Shivajirao S. Jondhale College of
+            Engineering {"& "}
+            Technology, Shahapur, Asangaon, Dist Thane
+          </h3>
+        </div>
       </div>
+      {/* <div className={styles.imgContainer}>
+        <Image src={educationImg} alt="" layout="fill" objectFit="contain" />
+      </div> */}
     </div>
   );
 };
